7c5ca0dcd006cc5e4122e62e28438d2ff9c8280e
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-08-05 15:24:35 +1000

    Add tests manager

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,6 +2,20 @@ const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
     it("Get the office number from the constructor", () => {
+        const officeNumber = 123456
+        const man = new Manager ("name", "id", "email", officeNumber)
+        expect(man.office).toBe(123456)
+    })
 
+    it("Office number must be numeric", () => {
+        const number = 1234567
+        const man = new Manager("name", "id", "email", number)
+        expect(man.validateNumber()).toBe(true)
+    })
+
+    it("Office number must be numeric", () => {
+        const number = "1234567e"
+        const man = new Manager("name", "id", "email", number)
+        expect(man.validateNumber()).toBe(false)
     })
 })
