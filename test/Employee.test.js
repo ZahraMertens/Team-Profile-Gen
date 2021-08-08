96d4b02703f79a2801cdd5d13a58a4acc9f5753d
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-08-08 21:21:45 +1000

    Add test for employee

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,37 +1,68 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () =>{
-    it("Gets name via cunstructor parameter", () => {
-        const name = "Jamie";
-        const paramName = new Employee("Jamie")
-        expect(paramName.name).toBe(name)
+
+    describe("Validate input for employee", () => {
+        
+        it("Valid type of email", () => {
+        const email = "hello@test.net";
+        const emp = new Employee("name", "id", email)
+        expect(emp.validateEmail()).toBe(true)
+        })
+    
+        it("Invalid type of email", () => {
+            const email = "hellotest.net";
+            const emp = new Employee("name", "id", email)
+            expect(emp.validateEmail()).toBe(false)
+        })
+
+        it("Validate if id contains special characters", () =>{
+            const id = "12Fg@"
+            const emp = new Employee("name", id)
+            expect(emp.validateId()).toBe(false)
+        })
+
+        it("Validate that ID only contains upper case and/or lower case letters and numbers", () =>{
+            const id = "12Fg"
+            const emp = new Employee("name", id)
+            expect(emp.validateId()).toBe(true)
+        })
     })
 
+    describe("Data type to be object", () => {
+        
+        it("Employee to be an object", () => {
+            const emp = new Employee();
+            expect(typeof(emp)).toBe("object")
+        })
+
+        it("Employee to be an object to be false", () => {
+            const emp = new Employee();
+            expect(emp).toMatchObject(emp)
+        })
+    })
+
+
+
+    it("Gets name via cunstructor parameter", () => {
+            const name = "Jamie";
+            const paramName = new Employee("Jamie")
+            expect(paramName.name).toBe(name)
+    })
     it("Gets ID via cunstructor parameter", () => {
         const idValue = 12;
         const paramId = new Employee("name", idValue)
         expect(paramId.id).toBe(idValue)
     })
 
-    it("Employee to be an object", () => {
-        const emp = new Employee();
-        expect(typeof(emp)).toBe("object")
-    })
+    
 
-    it("Valid type of email", () => {
-        const email = "hello@test.net";
-        const emp = new Employee("name", "id", email)
-        expect(emp.validateEmail()).toBe(true)
-    })
-
-    it("Invalid type of email", () => {
-        const email = "hellotest.net";
-        const emp = new Employee("name", "id", email)
-        expect(emp.validateEmail()).toBe(false)
-    })
+    
 
     it("getRole() returns 'Employee'", () => {
         const emp = new Employee("name", "id", "email")
         expect(emp.getRole()).toBe("Employee")
     })
+
+   
 })
