15b351e7bbeebf3f64e9249c7a199b7474ab5da4
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-08-09 14:24:26 +1000

    Add test for engieer

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -3,15 +3,49 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
 
-    it("Can get gitHub username from constructor", () => {
-        const username = "HelloWorld";
-        const eng = new Engineer("name", "id", "email", username)
-        expect(eng.getGitHub()).toBe(username)
+    describe("Get values from constructor", () => {
+
+        it("Can get gitHub username from constructor", () => {
+            const username = "HelloWorld";
+            const eng = new Engineer("name", "id", "email", username)
+            expect(eng.getGitHub()).toBe(username)
+        })
+
+        it("Can get name from constructor", () => {
+            const eng = new Engineer("Jamie")
+            expect(eng.name).toBe("Jamie")
+        })
+
+        it("Can get id from constructor", () => {
+            const eng = new Engineer("name", "1234d")
+            expect(eng.id).toBe("1234d")
+        })
+
+        it("Can get email from constructor", () => {
+            const eng = new Engineer("name", "id", "zahra@test.com")
+            expect(eng.email).toBe("zahra@test.com")
+        })
+
+        it("Can get role from class", () => {
+            const eng = new Engineer();
+            expect(eng.getRole()).toBe("Engineer")
+        })    
+    
     })
 
-    it("Can get role from class", () => {
-        const eng = new Engineer();
-        expect(eng.getRole()).toBe("Engineer")
+    describe("Validate user input", () => {
+
+        it("Email to match regex format", () => {
+            const email = "zahra@test.com";
+            const eng = new Engineer ("name", "id", email);
+            expect(eng.email).toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+        })
+
+        it("Email not to match regex email", () => {
+            const email = "zahratest.com";
+            const eng = new Engineer ("name", "id", email);
+            expect(eng.email).not.toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+        })
     })
 
 })
